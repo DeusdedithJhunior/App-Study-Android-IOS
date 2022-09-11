73bc1cecfc661dbe6b51f84789bb0d5142b62a4c
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-09-10 21:58:08 -0300

    creating QuestionPaperModel class

--- studyapp/lib/controllers/questions_paper/data_uploader.dart
+++ studyapp/lib/controllers/questions_paper/data_uploader.dart
@@ -1,5 +1,10 @@
 // ignore: depend_on_referenced_packages
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'dart:convert';
+
+import 'package:studyapp/models/question_paper_model.dart';
 
 class DataUploader extends GetxController {
   @override
@@ -8,7 +13,28 @@ class DataUploader extends GetxController {
     super.onReady();
   }
 
-  void uploadData() {
-    print('texte carregamento');
+  Future<void> uploadData() async {
+    final manifestContent = await DefaultAssetBundle.of(Get.context!)
+        .loadString('AssetManifest.json');
+    final Map<String, dynamic> manifestMap = json.decode(manifestContent);
+
+    //carregando arquivo json e monstrado o caminho
+    final papersInAssets = manifestMap.keys
+        .where((path) =>
+            path.startsWith('assets/dummy_data/papers') &&
+            path.contains('.json'))
+        .toList();
+
+    List<QuestionPaperModel> questionPapers = [];
+    // laço (interação) para acessar o conteudo de cada json da pasta papers
+    for (var paper in papersInAssets) {
+      String stringPaperContent = await rootBundle.loadString(paper);
+      questionPapers.add(
+        QuestionPaperModel.fromJson(
+          json.decode(stringPaperContent),
+        ),
+      );
+    }
+    print('quantidade: ${questionPapers.length}');
   }
 }

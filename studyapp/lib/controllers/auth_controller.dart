ca9e524c8baaee8c8e82738a7568e94e9a5ee839
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-09-13 19:29:59 -0300

    auth controller screen

---
+++ studyapp/lib/controllers/auth_controller.dart
@@ -0,0 +1,15 @@
+import 'package:get/get.dart';
+
+class AuthController extends GetxController {
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  // metodo que vai iniciar "na tela de abertura" do app
+  void initAuth(){
+
+  }
+
+  // metodo que vai nagevar para pagina de introdução do app
+}

905038cc8a56d906329ef184ec2e66ba8a1a5f99
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-09-13 19:31:54 -0300

    creating routes class

---
+++ studyapp/lib/routes/app_routes.dart
@@ -0,0 +1,8 @@
+import 'package:get/get.dart';
+import '../screens/splash/splash_screen.dart';
+
+class AppRoutes {
+  static List<GetPage> routes() => [
+        GetPage(name: '/', page: () => const SplashScreen()),
+      ];
+}

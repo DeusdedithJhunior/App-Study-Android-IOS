6176cbf17a5b38ea0d6bba598b4fe2a6c1b90960
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-09-13 20:14:29 -0300

    use theme in app

---
+++ studyapp/lib/configs/themes/app_colors.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:studyapp/configs/themes/app_dark_theme.dart';
+import 'package:studyapp/configs/themes/app_light_theme.dart';
+import 'package:studyapp/configs/themes/ui_parameters.dart';
+
+const mainGradientLight = LinearGradient(
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+    colors: [
+      primaryLightColorLight,
+      primaryColorLight,
+    ]);
+
+const mainGradientDark = LinearGradient(
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+    colors: [
+      primaryDarkColorDark,
+      primaryColorDark,
+    ]);
+
+// vai redenrizar a UI usando como base a classe UIParameters,
+// verificando se o config, do smartphone é dark ou light,
+// se o smartphone for dark, o theme do app será dark, se não, o app será ligth
+LinearGradient mainGraidient(BuildContext context) =>
+    UIParameters.isDarkMode(context) ? mainGradientDark : mainGradientLight;
--- studyapp/lib/configs/themes/ui_parameters.dart
+++ studyapp/lib/configs/themes/ui_parameters.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 // classe que vai verificar se o app está no dark ou light
+// se true retorna dark, se false, retorna ligth
 class UIParameters {
   static bool isDarkMode(BuildContext context) {
     return Theme.of(context).brightness == Brightness.dark;

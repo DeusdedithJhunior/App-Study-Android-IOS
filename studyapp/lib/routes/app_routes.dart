be057a27f1bdb5b20623b614ae0fe2dc27dbab7c
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-09-14 23:56:26 -0300

    settings routes

--- studyapp/lib/routes/app_routes.dart
+++ studyapp/lib/routes/app_routes.dart
@@ -1,10 +1,24 @@
 import 'package:get/get.dart';
+import 'package:studyapp/controllers/questions_papers/question_paper_controller.dart';
+import 'package:studyapp/screens/home/home_screen.dart';
 import 'package:studyapp/screens/introduction/introduction_screen.dart';
 import '../screens/splash/splash_screen.dart';
 
 class AppRoutes {
   static List<GetPage> routes() => [
-        GetPage(name: '/', page: () => const SplashScreen()),
-        GetPage(name: '/appintroduction', page: () => const IntroductionScreen())
+        GetPage(
+          name: '/',
+          page: () => const SplashScreen(),
+        ),
+        GetPage(
+          name: '/appintroduction',
+          page: () => const IntroductionScreen(),
+        ),
+        GetPage(
+            name: '/home',
+            page: () => const HomeScreen(),
+            binding: BindingsBuilder(() {
+              Get.put(QuestionPaperController());
+            })),
       ];
 }

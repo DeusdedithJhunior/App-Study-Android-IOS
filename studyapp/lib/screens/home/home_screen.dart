a533290c488d282fb99e433c50eebb84bc8765e2
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-09-14 23:56:51 -0300

    creating home screen

---
+++ studyapp/lib/screens/home/home_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:studyapp/controllers/questions_papers/question_paper_controller.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: no_leading_underscores_for_local_identifiers
+    QuestionPaperController _questionPaperController = Get.find();
+
+    return Scaffold(
+      body: ListView.separated(
+        itemBuilder: (BuildContext context, int index) {
+          return ClipRRect(
+            child: SizedBox(
+              height: 200,
+              width: 200,
+              child: FadeInImage(
+                  placeholder:
+                      const AssetImage('assets/imagens/app_splash_logo.png'),
+                  image: NetworkImage(
+                      _questionPaperController.allPaperImages[index])),
+            ),
+          );
+        },
+        itemCount: _questionPaperController.allPaperImages.length,
+        separatorBuilder: (BuildContext context, int index) {
+          return const SizedBox(height: 20);
+        },
+      ),
+    );
+  }
+}

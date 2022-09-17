f5c65480c3a07757dc6cac052ecb2c4bdadde1f3
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-09-17 20:35:11 -0300

    dependencies

--- studyapp/lib/bindings/initial_bindings.dart
+++ studyapp/lib/bindings/initial_bindings.dart
@@ -1,10 +1,13 @@
 import 'package:get/get.dart';
 import 'package:studyapp/controllers/auth_controller.dart';
 import 'package:studyapp/controllers/theme_controller.dart';
+import '../services/firebase_storage_services.dart';
 
 class InitialBindings implements Bindings {
   @override
   void dependencies() {
+    Get.lazyPut(() => FirebaseStorageService());
+    //Get.put(PapersDataUploader());
     Get.put(ThemeController());
     Get.put(AuthController(), permanent: true);
   }

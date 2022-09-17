34d3b3f623af44f5432268c7d5e984926098071b
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-09-17 20:35:15 -0300

    dependencies

--- studyapp/lib/main.dart
+++ studyapp/lib/main.dart
@@ -5,15 +5,19 @@
 // import 'firebase_options.dart';
 // import 'firebase_options.dart';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:studyapp/bindings/initial_bindings.dart';
 import 'package:studyapp/controllers/theme_controller.dart';
 import 'package:studyapp/routes/app_routes.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   InitialBindings().dependencies();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const Myapp());
 }
 

466123a39ce99fce6ae5ec07247f1def0e8f27c1
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-09-18 19:27:34 -0300

    responsive text style

--- studyapp/lib/configs/themes/custom_text_styles.dart
+++ studyapp/lib/configs/themes/custom_text_styles.dart
@@ -3,10 +3,11 @@ import 'ui_parameters.dart';
 
 // classe com o estilo de texto dos cards
 
-TextStyle cardTitles(context) => TextStyle(
-      color: UIParameters.isDarkMode()
-          ? Theme.of(context).textTheme.bodyText1!.color
-          : Theme.of(context).primaryColor,
-      fontSize: 18,
-      fontWeight: FontWeight.bold,
-    );
+TextStyle cartTitles(context) => TextStyle(
+    color: UIParameters.isDarkMode()
+        ? Theme.of(context).textTheme.bodyText1!.color
+        : Theme.of(context).primaryColor,
+    fontSize: 18,
+    fontWeight: FontWeight.bold);
+
+const detailText = TextStyle(fontSize: 12);

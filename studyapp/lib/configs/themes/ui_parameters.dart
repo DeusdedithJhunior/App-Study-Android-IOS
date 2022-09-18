fba766d78a837262d57433d1f6e7d2ea312c2c3d
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-09-18 18:52:16 -0300

    settings ui_parameters

--- studyapp/lib/configs/themes/ui_parameters.dart
+++ studyapp/lib/configs/themes/ui_parameters.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 
-// classe que vai verificar se o app está no dark ou light
-// se true retorna dark, se false, retorna ligth
+const double _mobileScreenPadding = 25.00;
+const double _cardBorderRadius = 10.00;
+
+double get mobileScreenPadding => _mobileScreenPadding;
+double get cardBorderRadius => _cardBorderRadius;
+
 class UIParameters {
+  static BorderRadius get cardBorderRadius =>
+      BorderRadius.circular(_cardBorderRadius);
+
+  static EdgeInsets get mobileScreenPadding =>
+      const EdgeInsets.all(_mobileScreenPadding);
+
+// vai verificar se o app está no dark ou light
+// se true retorna dark, se false, retorna ligth
   static bool isDarkMode(BuildContext context) {
     return Theme.of(context).brightness == Brightness.dark;
   }

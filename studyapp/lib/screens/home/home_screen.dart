ef20e5839648773aa0bd70be0cfc2711a37d07b1
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-09-19 01:56:16 -0300

    remove circle button

--- studyapp/lib/screens/home/home_screen.dart
+++ studyapp/lib/screens/home/home_screen.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_zoom_drawer/config.dart';
 import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
 import 'package:get/get.dart';
-import 'package:studyapp/components/app_circle_button.dart';
 import 'package:studyapp/components/content_area.dart';
 import 'package:studyapp/configs/themes/app_colors.dart';
 import 'package:studyapp/configs/themes/app_icons.dart';
 import 'package:studyapp/configs/themes/custom_text_styles.dart';
 import 'package:studyapp/controllers/questions_papers/question_paper_controller.dart';
 import 'package:studyapp/controllers/zoom_drawer_controller.dart';
+import 'package:studyapp/screens/home/zoom_menu_screen.dart';
 import '../../configs/themes/ui_parameters.dart';
 import 'question_card.dart';
 
@@ -21,74 +22,83 @@ class HomeScreen extends GetView<MyZoomDrawerController> {
     return Scaffold(
       body: GetBuilder<MyZoomDrawerController>(
         builder: (_) {
-          return ZoomDrawer(
-            menuScreen: const Text(
-              'Olá',
-              style: TextStyle(color: Colors.red),
-            ),
-            mainScreen: Container(
-              decoration: BoxDecoration(gradient: mainGraidient()),
-              child: SafeArea(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.all(mobileScreenPadding),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          AppCircleButton(
-                            onTap: controller.toogleDrawer,
-                            child: const Icon(AppIcons.menuLeft),
-                          ),
-                          const SizedBox(height: 10),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 10),
-                            child: Row(
-                              children: <Widget>[
-                                const Icon(AppIcons.greetingIcon),
-                                Text(
-                                  'Olá Estudante',
-                                  style: detailText.copyWith(
-                                      color: onSurfaceTextColor),
-                                ),
-                              ],
+          return Container(
+            decoration: BoxDecoration(gradient: mainGraidient()),
+            child: ZoomDrawer(
+              borderRadius: 50.0,
+              controller: _.zoomDrawerController,
+              showShadow: false,
+              angle: 0.0,
+              style: DrawerStyle.defaultStyle,
+              // menuBackgroundColor: Colors.grey.withOpacity(0.1),
+              slideWidth: MediaQuery.of(context).size.width * 0.8,
+              menuScreen: const MyZoomMenuScreen(),
+              mainScreen: Container(
+                decoration: BoxDecoration(gradient: mainGraidient()),
+                child: SafeArea(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(mobileScreenPadding),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            // AppCircleButton(
+                            //   onTap: controller.toogleDrawer,
+                            //   child: const Icon(AppIcons.menuLeft),
+                            // ),
+                            IconButton(
+                              onPressed: controller.toogleDrawer,
+                              icon: const Icon(AppIcons.menuLeft),
                             ),
-                          ),
-                          const Text(
-                            'O que você quer aprender hoje?',
-                            style: headerText,
-                          ),
-                        ],
+                            const SizedBox(height: 10),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 10),
+                              child: Row(
+                                children: <Widget>[
+                                  const Icon(AppIcons.greetingIcon),
+                                  Text('Olá Estudante',
+                                      style: detailText.copyWith(
+                                          color: onSurfaceTextColor)),
+                                ],
+                              ),
+                            ),
+                            const Text(
+                              'O que você quer aprender hoje?',
+                              style: headerText,
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        child: ContentArea(
-                          addPadding: false,
-                          child: Obx(
-                            () => ListView.separated(
-                              padding: UIParameters.mobileScreenPadding,
-                              shrinkWrap: true,
-                              itemBuilder: (BuildContext context, int index) {
-                                return QuestionCard(
-                                  model:
-                                      _questionPaperController.allPaper[index],
-                                );
-                              },
-                              itemCount:
-                                  _questionPaperController.allPaper.length,
-                              separatorBuilder:
-                                  (BuildContext context, int index) {
-                                return const SizedBox(height: 20);
-                              },
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: ContentArea(
+                            addPadding: false,
+                            child: Obx(
+                              () => ListView.separated(
+                                padding: UIParameters.mobileScreenPadding,
+                                shrinkWrap: true,
+                                itemBuilder: (BuildContext context, int index) {
+                                  return QuestionCard(
+                                    model: _questionPaperController
+                                        .allPaper[index],
+                                  );
+                                },
+                                itemCount:
+                                    _questionPaperController.allPaper.length,
+                                separatorBuilder:
+                                    (BuildContext context, int index) {
+                                  return const SizedBox(height: 20);
+                                },
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),

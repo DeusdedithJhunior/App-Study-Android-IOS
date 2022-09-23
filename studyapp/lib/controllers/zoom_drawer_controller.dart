46f0a045b63b9b85d4fbc4108007c3ca90a9a4f6
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-09-22 23:08:01 -0300

    setting zoon controller

--- studyapp/lib/controllers/zoom_drawer_controller.dart
+++ studyapp/lib/controllers/zoom_drawer_controller.dart
@@ -1,11 +1,36 @@
 import 'package:flutter_zoom_drawer/config.dart';
 import 'package:get/get.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MyZoomDrawerController extends GetxController {
   final zoomDrawerController = ZoomDrawerController();
 
+  @override
+  void onReady() {
+    
+    super.onReady();
+  }
+
   void toogleDrawer() {
     zoomDrawerController.toggle?.call();
     update();
+  }
+
+  void signOut() {}
+
+  void signIn() {}
+
+  void webSite() {}
+
+  void email() {
+    final Uri emailLaunchUri =
+        Uri(scheme: 'DeusdeditJunior', path: 'deusdedit.junior@gmail.com');
+    _launch(emailLaunchUri.toString());
+  }
+
+  Future<void> _launch(String url) async {
+    if (!await launch(url)) {
+      throw 'Não foi possível iniciar $url';
+    }
   }
 }

c9566364592eb65f21a204c369947527f1f2b361
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-09-22 23:07:22 -0300

    add firebase auth

--- studyapp/lib/controllers/auth_controller.dart
+++ studyapp/lib/controllers/auth_controller.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthController extends GetxController {
   @override
@@ -7,11 +9,45 @@ class AuthController extends GetxController {
     super.onReady();
   }
 
+  late FirebaseAuth _auth;
+  final _user = Rxn<User>();
+  late Stream<User?> _authStateChanges;
+
   // metodo que vai iniciar "na tela de abertura" do app
   void initAuth() async {
     await Future.delayed(const Duration(seconds: 2));
+    _auth = FirebaseAuth.instance;
+    _authStateChanges = _auth.authStateChanges();
+    _authStateChanges.listen((User? user) {
+      _user.value = user;
+    });
     navigateToIntroduction();
   }
+
+  // login com google
+  singInWithGoogle() async {
+    final GoogleSignIn _googleSingIn = GoogleSignIn();
+    try {
+      GoogleSignInAccount? account = await _googleSingIn.signIn();
+      if (account != null) {
+        final _authAccount = await account.authentication;
+        final _credential = GoogleAuthProvider.credential(
+            idToken: _authAccount.idToken,
+            accessToken: _authAccount.accessToken);
+
+        await _auth.signInWithCredential(_credential);
+        await saveUser(account);
+      }
+    } on Exception catch (error) {
+      // ignore: avoid_print
+      print(error);
+    }
+  }
+
+  saveUser(GoogleSignInAccount account){
+    
+  }
+
 
   // metodo que vai nagevar para pagina de introdução do app
   void navigateToIntroduction() {

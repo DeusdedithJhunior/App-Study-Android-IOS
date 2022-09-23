a6f94998c58881c918b9e25cf011f97b395c1e3d
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-09-23 17:49:17 -0300

    setting GetView

--- studyapp/lib/screens/home/question_card.dart
+++ studyapp/lib/screens/home/question_card.dart
@@ -4,11 +4,11 @@ import 'package:get/get.dart';
 import 'package:studyapp/configs/themes/app_icons.dart';
 import 'package:studyapp/configs/themes/ui_parameters.dart';
 import 'package:studyapp/models/question_paper_model.dart';
-
 import '../../components/app_icon_text.dart';
 import '../../configs/themes/custom_text_styles.dart';
+import '../../controllers/questions_papers/question_paper_controller.dart';
 
-class QuestionCard extends StatelessWidget {
+class QuestionCard extends GetView<QuestionPaperController> {
   final QuestionPaperModel model;
 
   const QuestionCard({
@@ -24,102 +24,107 @@ class QuestionCard extends StatelessWidget {
       decoration: BoxDecoration(
           borderRadius: UIParameters.cardBorderRadius,
           color: Theme.of(context).cardColor),
-      child: Padding(
-        padding: const EdgeInsets.all(_padding),
-        child: Stack(
-          clipBehavior: Clip.none,
-          children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: ColoredBox(
-                    color: Theme.of(context).primaryColor.withOpacity(0.1),
-                    child: SizedBox(
-                      height: Get.width * 0.15,
-                      width: Get.width * 0.15,
-                      child: CachedNetworkImage(
-                        imageUrl: model.imageUrl,
-                        placeholder: (context, url) => Container(
-                          alignment: Alignment.center,
-                          child: const CircularProgressIndicator(),
+      child: InkWell(
+        onTap: () {
+          controller.navigateToQuestions(paper: model);
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(_padding),
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: ColoredBox(
+                      color: Theme.of(context).primaryColor.withOpacity(0.1),
+                      child: SizedBox(
+                        height: Get.width * 0.15,
+                        width: Get.width * 0.15,
+                        child: CachedNetworkImage(
+                          imageUrl: model.imageUrl,
+                          placeholder: (context, url) => Container(
+                            alignment: Alignment.center,
+                            child: const CircularProgressIndicator(),
+                          ),
+                          errorWidget: (context, url, error) =>
+                              Image.asset('assets/images/app_splash_logo.png'),
                         ),
-                        errorWidget: (context, url, error) =>
-                            Image.asset('assets/images/app_splash_logo.png'),
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(width: 12),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(model.title, style: cartTitles(context)),
-                      Padding(
-                          padding: const EdgeInsets.only(top: 10, bottom: 15),
-                          child: Text(model.description)),
-                      Row(
-                        children: [
-                          AppIconText(
-                            icon: Icon(
-                              Icons.help_outline_sharp,
-                              color: Get.isDarkMode
-                                  ? Colors.white
-                                  : Theme.of(context).primaryColor,
-                            ),
-                            text: Text(
-                              '${model.questionCount} questões',
-                              style: detailText.copyWith(
+                  const SizedBox(width: 12),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(model.title, style: cartTitles(context)),
+                        Padding(
+                            padding: const EdgeInsets.only(top: 10, bottom: 15),
+                            child: Text(model.description)),
+                        Row(
+                          children: [
+                            AppIconText(
+                              icon: Icon(
+                                Icons.help_outline_sharp,
                                 color: Get.isDarkMode
                                     ? Colors.white
                                     : Theme.of(context).primaryColor,
                               ),
+                              text: Text(
+                                '${model.questionCount} questões',
+                                style: detailText.copyWith(
+                                  color: Get.isDarkMode
+                                      ? Colors.white
+                                      : Theme.of(context).primaryColor,
+                                ),
+                              ),
                             ),
-                          ),
-                          const SizedBox(width: 15),
-                          AppIconText(
-                            icon: Icon(
-                              Icons.timer,
-                              color: Get.isDarkMode
-                                  ? Colors.white
-                                  : Theme.of(context).primaryColor,
-                            ),
-                            text: Text(
-                              model.timeInMinits(),
-                              style: detailText.copyWith(
+                            const SizedBox(width: 15),
+                            AppIconText(
+                              icon: Icon(
+                                Icons.timer,
                                 color: Get.isDarkMode
                                     ? Colors.white
                                     : Theme.of(context).primaryColor,
                               ),
+                              text: Text(
+                                model.timeInMinits(),
+                                style: detailText.copyWith(
+                                  color: Get.isDarkMode
+                                      ? Colors.white
+                                      : Theme.of(context).primaryColor,
+                                ),
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                    ],
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            ),
-            Positioned(
-              bottom: -_padding, //sinal de menos
-              right: -_padding,
-              child: GestureDetector(
-                child: Container(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(cardBorderRadius),
-                        bottomRight: Radius.circular(cardBorderRadius)),
+                ],
+              ),
+              Positioned(
+                bottom: -_padding, //sinal de menos
+                right: -_padding,
+                child: GestureDetector(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 12, horizontal: 20),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(cardBorderRadius),
+                          bottomRight: Radius.circular(cardBorderRadius)),
+                    ),
+                    child: const Icon(AppIcons.trophyOutLine),
                   ),
-                  child: const Icon(AppIcons.trophyOutLine),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );

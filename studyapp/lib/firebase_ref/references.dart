c49ace00ee948e11cdce40a3828257414b34f1e5
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-09-23 17:49:33 -0300

    creating collection users

--- studyapp/lib/firebase_ref/references.dart
+++ studyapp/lib/firebase_ref/references.dart
@@ -1,11 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
+Reference get firebaseStorage => FirebaseStorage.instance.ref();
 final fireStore = FirebaseFirestore.instance;
+
+final userRF = fireStore.collection('users');
+
 final questionPaperRF = fireStore.collection('questionPapers');
 DocumentReference questionRF({
   required String paperId,
   required String questionId,
 }) =>
     questionPaperRF.doc(paperId).collection('question').doc(questionId);
-Reference get firebaseStorage => FirebaseStorage.instance.ref();

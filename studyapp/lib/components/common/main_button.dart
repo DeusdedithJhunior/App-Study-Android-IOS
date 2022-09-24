0dfeaca4df197efb654aef4723a712d37b1ccd24
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-09-23 22:14:17 -0300

    create main button

---
+++ studyapp/lib/components/common/main_button.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:studyapp/configs/themes/app_colors.dart';
+
+class MainButton extends StatelessWidget {
+  final String title;
+  final VoidCallback onTap;
+  // ignore: prefer_typing_uninitialized_variables
+  final enabled;
+  final Widget? child;
+  final Color? color;
+
+  const MainButton({
+    super.key,
+    this.title = '',
+    required this.onTap,
+    this.enabled = true,
+    this.child,
+    this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      type: MaterialType.transparency,
+      child: SizedBox(
+        height: 55,
+        child: InkWell(
+          onTap: enabled == false ? null : onTap,
+          child: Container(
+            width: double.maxFinite,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: color ?? Theme.of(context).cardColor),
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: child ??
+                  Center(
+                    child: Text(
+                      title,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: onSurfaceTextColor),
+                    ),
+                  ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
